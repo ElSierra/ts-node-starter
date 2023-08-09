e223315cafa0dfa91335068421f10f094cfb335d
Isaac Ojo <hojoisaac@gmail.com>
2023-08-09 13:03:58 +0100

    restructure

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,13 @@ import express from "express";
 import bodyParser from "body-parser";
 import morgan from "morgan";
 import cors from "cors";
-import router from "./router";
+import router from "./routes/global";
 
 const app = express();
 app.use(cors());
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api", router);
+app.use("/", router);
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 import app from "./app";
+import notifier from "node-notifier";
 
 const Port = process.env.PORT || 5500;
 
 app.listen(Port, () => {
   console.log("🚀 Server Started @:", Port);
+  notifier.notify({
+    title: "Server started",
+    message: Port.toString(),
+  });
 });
